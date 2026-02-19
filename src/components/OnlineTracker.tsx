14a39b984829e56e2d5f09536c65bdcfb6c3b4f0
faragit <80652108+faragit@users.noreply.github.com>
2026-02-19 17:36:38 -0300

    Update OnlineTracker.tsx

--- src/components/OnlineTracker.tsx
+++ src/components/OnlineTracker.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "@/supabase";
 import { joinOnlinePresence } from "@/lib/onlinePresence";
 
 export default function OnlineTracker() {
